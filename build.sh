d34c54351c868f5f4bf9d3dc3ce300883f1c41f4
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2023-12-02 03:52:30 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -126,7 +126,7 @@ make
 sudo make install && cd $DIR
 
 git clone https://github.com/libjpeg-turbo/libjpeg-turbo/ turbo && cd turbo
-CMAKE_INSTALL_PREFIX="/usr" cmake -G"Unix Makefiles"
+cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=/usr
 make
 sudo make install && cd $DIR
 
